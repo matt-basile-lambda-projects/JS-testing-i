40aa3c90d0ef9d8bcb33c7e014413e37119e76f9
Matt Basile <matthewbasile2@gmail.com>
2019-03-04 16:21:41 -0500

    Item Title Functionality added with a basic test implemented.

--- enhancer/enhancer.js
+++ enhancer/enhancer.js
@@ -1,11 +1,58 @@
 module.exports ={
     repair,
-    
+    itemTitle
 }
 
-function repair(object){
+const armor = {
+    name: 'Shield',
+    type: 'armor',
+    durability: 100,
+    enhancement: 0
+}
+const weapon = {
+    name: 'Sword',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 0
+}
+
+
+function repair(item){
     return {
-        ...object,
+        ...item,
         durability: 100,
     }
+}
+
+function enhancer(item){
+    // Item Enhancement Caps
+
+
+    
+    //item.enhacement <= 20 
+    //item.durability <= 100
+    // item.enahncement <15 ? item.durability >= 20 : null
+    // 19>= item.enahncement >=15 ? item.durability >= 0 : null
+}
+
+// item.enhancement === 0
+// item.enhancement <= 20
+// item.type armor cannot fail before item.enhancement = 5
+// item.type weapon cannot fail before item.enhancement = 7
+// item.name = item.enhancement === 0 ? { ...item, name: item.name } :{ ...item, name: [`+${item.enhancement}`]item.name } 
+
+
+//Item Durability caps
+
+
+//Item type function
+// item.type === armor || weapon
+
+//Item title function
+function itemTitle(item){
+    let title = item.name;
+    let level = item.enhancement;
+    level === 16 ? level = "PRI" : level === 17 ? level = "DUO" : level === 18 ? level = "TRI" : level === 19 ? level = "TET" : level === 20 ? level = "PEN" : null;
+    return level === 0 ? { ...item, name: title } : item.enhancement > 15 ? { ...item, name: `[${level}]${title}`} :  { ...item, name: `[+${level}]${title}`} 
+    
 }
--- enhancer/enhancer.test.js
+++ enhancer/enhancer.test.js
@@ -6,3 +6,39 @@ test('repair', ()=>{
     }
     expect(enhancer.repair(item)).toEqual({durability:100})
 })
+
+test('title PRI', ()=>{
+    const item ={
+        enhancement: 16,
+        name: 'Shield'
+    }
+    expect(enhancer.itemTitle(item)).toEqual({name: '[PRI]Shield', enhancement: 16})
+})
+test('title DUO', ()=>{
+    const item ={
+        enhancement: 17,
+        name: 'Shield'
+    }
+    expect(enhancer.itemTitle(item)).toEqual({name: '[DUO]Shield', enhancement: 17})
+})
+test('title TRI', ()=>{
+    const item ={
+        enhancement: 18,
+        name: 'Shield'
+    }
+    expect(enhancer.itemTitle(item)).toEqual({name: '[TRI]Shield', enhancement: 18})
+})
+test('title TET', ()=>{
+    const item ={
+        enhancement: 19,
+        name: 'Shield'
+    }
+    expect(enhancer.itemTitle(item)).toEqual({name: '[TET]Shield', enhancement: 19})
+})
+test('title PEN', ()=>{
+    const item ={
+        enhancement: 20,
+        name: 'Shield'
+    }
+    expect(enhancer.itemTitle(item)).toEqual({name: '[PEN]Shield', enhancement: 20})
+})
