88d670ca06a287c56785b0d9e19abeab30533cb3
Matt Basile <matthewbasile2@gmail.com>
2019-03-04 17:37:14 -0500

    Added success functionality with some testing implemented. Finding an error with fringe cases that I'm working on now.

--- enhancer/enhancer.js
+++ enhancer/enhancer.js
@@ -1,6 +1,10 @@
 module.exports ={
     repair,
-    itemTitle
+    itemTitle,
+    enhancerFinal,
+    itemType,
+    durabilityRange,
+    enhanceRange
 }
 
 const armor = {
@@ -24,35 +28,91 @@ function repair(item){
     }
 }
 
-function enhancer(item){
+function enhancerFinal(item){
     // Item Enhancement Caps
-
-
     
-    //item.enhacement <= 20 
-    //item.durability <= 100
-    // item.enahncement <15 ? item.durability >= 20 : null
-    // 19>= item.enahncement >=15 ? item.durability >= 0 : null
+    const type = itemType(item.type)
+    
+    const eRange = enhanceRange(item.enhancement)
+    
+    const dRange = durabilityRange(item.durability, item.enhancement)
+   
+    if(type && eRange && dRange){
+        console.log(type)
+        console.log(eRange)
+        console.log(dRange)
+        return succeeds({
+            ...item,
+            type,
+            durability: dRange,
+            enhancement: eRange
+        })
+    } else{
+        return fail()
+    }
 }
 
-// item.enhancement === 0
-// item.enhancement <= 20
-// item.type armor cannot fail before item.enhancement = 5
-// item.type weapon cannot fail before item.enhancement = 7
-// item.name = item.enhancement === 0 ? { ...item, name: item.name } :{ ...item, name: [`+${item.enhancement}`]item.name } 
 
+function durabilityRange(durability, enhancement){
+    if(durability >=0 && durability<=100){
+        if(enhancement >= 0 && enhancement <= 14){
+            if(durability >=20){
+                return durability
+            }
+        } else if(enhancement >= 15 && enhancement <= 20){
+            if(durability >=0){
+                return durability
+            }
+        } else {
+            return false
+        }
+    } else{
+        return false
+    }
+}
 
 //Item Durability caps
-
+function enhanceRange(enhancement){
+    if(enhancement >=0 && enhancement <=20){
+        return enhancement
+    } else{
+        return false
+    }
+}
 
 //Item type function
 // item.type === armor || weapon
+function itemType(type){
+    if(type === 'armor' || type === 'weapon'){
+        return type
+    } else{
+     return fail()
+    }
+}
+
 
 //Item title function
 function itemTitle(item){
     let title = item.name;
     let level = item.enhancement;
     level === 16 ? level = "PRI" : level === 17 ? level = "DUO" : level === 18 ? level = "TRI" : level === 19 ? level = "TET" : level === 20 ? level = "PEN" : null;
-    return level === 0 ? { ...item, name: title } : item.enhancement > 15 ? { ...item, name: `[${level}]${title}`} :  { ...item, name: `[+${level}]${title}`} 
+    return level === 0 ?   title  : item.enhancement > 15 ?  `[${level}]${title}` :  `[+${level}]${title}` 
     
+}
+function succeeds(item){
+    console.log(item)
+    let title = itemTitle(item)
+    console.log(title)
+    let enhance = item.enhancement + 1;
+    console.log(enhance)
+    const updated = {
+        ...item,
+        name: title,
+        enhancement: enhance
+    }
+    return updated
+}
+
+function fail(){
+    return null
 }
--- enhancer/enhancer.test.js
+++ enhancer/enhancer.test.js
@@ -1,44 +1,95 @@
 const enhancer = require('./enhancer');
 
-test('repair', ()=>{
-    const item = {
-        durability: 12
-    }
-    expect(enhancer.repair(item)).toEqual({durability:100})
+// test('repair', ()=>{
+//     const item = {
+//         durability: 12
+//     }
+//     expect(enhancer.repair(item)).toEqual({durability:100})
+// })
+
+// test('title PRI', ()=>{
+//     const item ={
+//         enhancement: 16,
+//         name: 'Shield'
+//     }
+//     expect(enhancer.itemTitle(item)).toBe( '[PRI]Shield')
+// })
+// test('title DUO', ()=>{
+//     const item ={
+//         enhancement: 17,
+//         name: 'Shield'
+//     }
+//     expect(enhancer.itemTitle(item)).toBe( '[DUO]Shield')
+// })
+// test('title TRI', ()=>{
+//     const item ={
+//         enhancement: 18,
+//         name: 'Shield'
+//     }
+//     expect(enhancer.itemTitle(item)).toBe( '[TRI]Shield')
+// })
+// test('title TET', ()=>{
+//     const item ={
+//         enhancement: 19,
+//         name: 'Shield'
+//     }
+//     expect(enhancer.itemTitle(item)).toBe( '[TET]Shield')
+// })
+// test('title PEN', ()=>{
+//     const item ={
+//         enhancement: 20,
+//         name: 'Shield'
+//     }
+//     expect(enhancer.itemTitle(item)).toBe( '[PEN]Shield')
+// })
+
+// test('successful update', ()=>{
+//     item={
+//         name: 'Shield',
+//         durability: 100,
+//         enhancement: 17,
+//         type: 'armor'
+//     }
+//     expect(enhancer.enhancer(item)).toEqual({
+//         name: '[TRI]Shield',
+//         durability: 100,
+//         enhancement: 18,
+//         type:'armor'
+//     })
+// })
+
+test('item type', ()=>{
+    expect(enhancer.itemType('armor')).toEqual('armor')
+    expect(enhancer.itemType('weapon')).toEqual('weapon')
+    expect(enhancer.itemType('blah')).toBe(null);
 })
 
-test('title PRI', ()=>{
-    const item ={
-        enhancement: 16,
-        name: 'Shield'
-    }
-    expect(enhancer.itemTitle(item)).toEqual({name: '[PRI]Shield', enhancement: 16})
+test('durability Range', ()=>{
+    expect(enhancer.durabilityRange(75, 7)).toBe(75)
+    expect(enhancer.durabilityRange(75, 16)).toBe(75)
+    expect(enhancer.durabilityRange(75, -1)).toBe(false)
+    expect(enhancer.durabilityRange(75, 101)).toBe(false)
 })
-test('title DUO', ()=>{
-    const item ={
-        enhancement: 17,
-        name: 'Shield'
-    }
-    expect(enhancer.itemTitle(item)).toEqual({name: '[DUO]Shield', enhancement: 17})
+test('enhancement Range', ()=>{
+    expect(enhancer.enhanceRange(7)).toBe(7)
+    expect(enhancer.enhanceRange(16)).toBe(16)
+    expect(enhancer.enhanceRange(20)).toBe(20)
+    expect(enhancer.enhanceRange(21)).toBe(false)
+    expect(enhancer.enhanceRange(-1)).toBe(false)
+    expect(enhancer.enhanceRange(101)).toBe(false)
 })
-test('title TRI', ()=>{
-    const item ={
-        enhancement: 18,
-        name: 'Shield'
+
+test('successful enhancement', ()=>{
+    const item = {
+        name: 'Shield',
+        type: 'armor',
+        durability: 100,
+        enhancement: 1
     }
-    expect(enhancer.itemTitle(item)).toEqual({name: '[TRI]Shield', enhancement: 18})
-})
-test('title TET', ()=>{
-    const item ={
-        enhancement: 19,
-        name: 'Shield'
-    }
-    expect(enhancer.itemTitle(item)).toEqual({name: '[TET]Shield', enhancement: 19})
-})
-test('title PEN', ()=>{
-    const item ={
-        enhancement: 20,
-        name: 'Shield'
-    }
-    expect(enhancer.itemTitle(item)).toEqual({name: '[PEN]Shield', enhancement: 20})
+    expect(enhancer.enhancerFinal(item)).toEqual({
+            name: '[+1]Shield',
+            type: 'armor',
+            durability: 100,
+            enhancement: 2
+    })
 })
